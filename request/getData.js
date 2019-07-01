9587110d723ee4eeb7c50477dae5f50456d47c2f
Ivan <ivohavasi@gmail.com>
2019-07-01 21:59:58 +0200

    set up config vars

--- request/getData.js
+++ request/getData.js
@@ -3,7 +3,7 @@ const {
 } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgres://jvdgmaaklzropg:a07975a87d6140993195484852c45bb25db52135d53de31f09e82bf82cbb6e76@ec2-54-228-252-67.eu-west-1.compute.amazonaws.com:5432/d1qttojp2t6sab',
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
 });
 
